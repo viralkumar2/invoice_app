4a59d9ec8f533e16f316c275e0f5cef61c6a86e0
unknown <viralode95745@gmail.com>
2023-11-17 16:37:39 +0530

    work on theme anaylasis work

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -1,5 +1,63 @@
 @extends('admin.app_master')
 
+<style>
+    * { box-sizing: border-box; }
+
+/* force scrollbar */
+html { overflow-y: scroll; }
+
+body { font-family: sans-serif; }
+
+/* ---- grid ---- */
+
+.grid {
+  max-width: 100%px;
+  background: #DDD;
+}
+
+/* clear fix */
+.grid:after {
+  content: '';
+  display: block;
+  clear: both;
+}
+
+/* ---- .grid-item ---- */
+
+.grid-sizer,
+.grid-item {
+  width: 25%;
+}
+
+.grid-item {
+  padding-bottom:0%; /* hack for proportional sizing */
+  float: left;
+  background-position: center center;
+  background-size: cover;
+}
+
+.grid-item--width2 {
+  width: 50%;
+}
+
+.grid-item--large {
+  width: 50%;
+  padding-bottom: 50%;
+}
+
+.packery-drop-placeholder {
+  border: 3px dotted #333;
+  background: hsla(0, 0%, 0%, 0.3);
+}
+
+.grid-item.is-dragging,
+.grid-item.is-positioning-post-drag {
+  z-index: 2;
+}
+
+</style>
+
+
 @section('content')
 
 
@@ -22,10 +80,169 @@
     </div>
     <section class="content">
       <div class="container-fluid">
+        <div class="grid">
+            <div class="grid-sizer">
+
+        </div>
+        <div class="grid-item grid-item" data-item-id="1">
+
+            <div class="col-lg-12 col-12">
+                <div class="small-box bg-info">
+                  <div class="inner">
+                    <h3>150</h3>
+
+                    <p>New Orders</p>
+                  </div>
+                  <div class="icon">
+                    <i class="ion ion-bag"></i>
+                  </div>
+                  <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="grid-item" data-item-id="2" >
+
+            <div class="col-lg-12 col-12">
+
+                <div class="small-box bg-success">
+                  <div class="inner">
+                    <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+                    <p>Bounce Rate</p>
+                  </div>
+                  <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                  </div>
+                  <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+              </div>
+
+        </div>
+        <div class="grid-item" data-item-id="3">
+
+            <div class="col-lg-12 col-12">
+
+                <div class="small-box bg-warning">
+                  <div class="inner">
+                    <h3>44</h3>
+
+                    <p>User Registrations</p>
+                  </div>
+                  <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                  </div>
+                  <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+              </div>
+
+        </div>
+
+        <div class="grid-item grid-item" data-item-id="4">
+        </div>
+        <div class="grid-item" data-item-id="5">
+
+            <div class="col-lg-12 col-12">
+                <div class="small-box bg-danger">
+                  <div class="inner">
+                    <h3>65</h3><p>Unique Visitors</p>
+                  </div>
+                  <div class="icon">
+                    <i class="ion ion-pie-graph"></i>
+                  </div>
+                  <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-6 grid-item grid-item" data-item-id="6">
+
+            <section class="col-lg-12 col-12 connectedSortable">
+                <div class="card">
+                  <div class="card-header">
+                    <h3 class="card-title">
+                      <i class="fas fa-chart-pie mr-1"></i>
+                      Sales
+                    </h3>
+                    <div class="card-tools">
+                      <ul class="nav nav-pills ml-auto">
+                        <li class="nav-item">
+                          <a class="nav-link active" href="#revenue-chart" data-toggle="tab">Area</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#sales-chart" data-toggle="tab">Donut</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    <div class="tab-content p-0">
+
+                      <div class="chart tab-pane active" id="revenue-chart"
+                           style="position: relative; height: 300px;">
+                          <canvas id="revenue-chart-canvas" height="300" style="height: 300px;"></canvas>
+                       </div>
+                      <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;">
+                        <canvas id="sales-chart-canvas" height="300" style="height: 300px;"></canvas>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+        </div>
+        <div class=" col-lg-6 grid-item" data-item-id="7">
+            <section class="col-lg-12 col-12 connectedSortable">
+                <div class="card bg-gradient-primary">
+                  <div class="card-header border-0">
+                    <h3 class="card-title">
+                      <i class="fas fa-map-marker-alt mr-1"></i>
+                      Visitors
+                    </h3>
+                    <div class="card-tools">
+                      <button type="button" class="btn btn-primary btn-sm daterange" title="Date range">
+                        <i class="far fa-calendar-alt"></i>
+                      </button>
+                      <button type="button" class="btn btn-primary btn-sm" data-card-widget="collapse" title="Collapse">
+                        <i class="fas fa-minus"></i>
+                      </button>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    <div id="world-map" style="height: 250px; width: 100%;"></div>
+                  </div>
+                  <div class="card-footer bg-transparent">
+                    <div class="row">
+                      <div class="col-4 text-center">
+                        <div id="sparkline-1"></div>
+                        <div class="text-white">Visitors</div>
+                      </div>
+
+                      <div class="col-4 text-center">
+                        <div id="sparkline-2"></div>
+                        <div class="text-white">Online</div>
+                      </div>
+
+                      <div class="col-4 text-center">
+                        <div id="sparkline-3"></div>
+                        <div class="text-white">Sales</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+
+        </div>
+        <div class="grid-item" data-item-id="8"
+            style="background-image: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/contrail.jpg);">
+        </div>
+        <div class="grid-item" data-item-id="9"
+            style="background-image: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/flight-formation.jpg);">
+        </div>
+        </div>
+
 
         <div class="row">
-          <div class="col-lg-3 col-6">
-
+          {{-- <div class="col-lg-3 col-6">
             <div class="small-box bg-info">
               <div class="inner">
                 <h3>150</h3>
@@ -37,9 +254,9 @@
               </div>
               <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-          </div>
+          </div> --}}
 
-          <div class="col-lg-3 col-6">
+          {{-- <div class="col-lg-3 col-6">
 
             <div class="small-box bg-success">
               <div class="inner">
@@ -52,9 +269,9 @@
               </div>
               <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-          </div>
+          </div> --}}
 
-          <div class="col-lg-3 col-6">
+          {{-- <div class="col-lg-3 col-6">
 
             <div class="small-box bg-warning">
               <div class="inner">
@@ -67,9 +284,9 @@
               </div>
               <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-          </div>
+          </div> --}}
 
-          <div class="col-lg-3 col-6">
+          {{-- <div class="col-lg-3 col-6">
             <div class="small-box bg-danger">
               <div class="inner">
                 <h3>65</h3>
@@ -81,11 +298,11 @@
               </div>
               <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-          </div>
+          </div> --}}
 
         </div>
         <div class="row">
-          <section class="col-lg-7 connectedSortable">
+          {{-- <section class="col-lg-7 connectedSortable">
             <div class="card">
               <div class="card-header">
                 <h3 class="card-title">
@@ -116,8 +333,8 @@
                 </div>
               </div>
             </div>
-          </section>
-          <section class="col-lg-5 connectedSortable">
+          </section> --}}
+          {{-- <section class="col-lg-5 connectedSortable">
             <div class="card bg-gradient-primary">
               <div class="card-header border-0">
                 <h3 class="card-title">
@@ -155,11 +372,92 @@
                 </div>
               </div>
             </div>
-          </section>
+          </section> --}}
         </div>
       </div>
     </section>
   </div>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://npmcdn.com/packery@2/dist/packery.pkgd.js"></script>
+    <script src="https://npmcdn.com/draggabilly@2/dist/draggabilly.pkgd.js"></script>
+  <script>
+    // external js: packery.pkgd.js, draggabilly.pkgd.js
+
+// add Packery.prototype methods
+
+// get JSON-friendly data for items positions
+Packery.prototype.getShiftPositions = function( attrName ) {
+  attrName = attrName || 'id';
+  var _this = this;
+  return this.items.map( function( item ) {
+    return {
+      attr: item.element.getAttribute( attrName ),
+      x: item.rect.x / _this.packer.width
+    }
+  });
+};
+
+Packery.prototype.initShiftLayout = function( positions, attr ) {
+  if ( !positions ) {
+    // if no initial positions, run packery layout
+    this.layout();
+    return;
+  }
+  // parse string to JSON
+  if ( typeof positions == 'string' ) {
+    try {
+      positions = JSON.parse( positions );
+    } catch( error ) {
+      console.error( 'JSON parse error: ' + error );
+      this.layout();
+      return;
+    }
+  }
+
+  attr = attr || 'id'; // default to id attribute
+  this._resetLayout();
+  // set item order and horizontal position from saved positions
+  this.items = positions.map( function( itemPosition ) {
+    var selector = '[' + attr + '="' + itemPosition.attr  + '"]'
+    var itemElem = this.element.querySelector( selector );
+    var item = this.getItem( itemElem );
+    item.rect.x = itemPosition.x * this.packer.width;
+    return item;
+  }, this );
+  this.shiftLayout();
+};
+
+// -----------------------------//
+
+// init Packery
+var $grid = $('.grid').packery({
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true,
+  initLayout: false // disable initial layout
+});
+
+// get saved dragged positions
+var initPositions = localStorage.getItem('dragPositions');
+// init layout with saved positions
+$grid.packery( 'initShiftLayout', initPositions, 'data-item-id' );
+
+// make draggable
+$grid.find('.grid-item').each( function( i, itemElem ) {
+  var draggie = new Draggabilly( itemElem );
+  $grid.packery( 'bindDraggabillyEvents', draggie );
+});
+
+// save drag positions on event
+$grid.on( 'dragItemPositioned', function() {
+  // save drag positions
+  var positions = $grid.packery( 'getShiftPositions', 'data-item-id' );
+  localStorage.setItem( 'dragPositions', JSON.stringify( positions ) );
+});
+
+
+</script>
+
   @endsection
 
 
